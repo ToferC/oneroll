21c520d8f5375cae39117ffcf9f20e45a8d01b09
toferc <cgeist7@gmail.com>
2018-06-09 13:46:45 -0400

    refactoring and splitting up power elements, combine extras & flaws into modifiers and add standard modifiers

--- tests/test_powers.go
+++ tests/test_powers.go
@@ -15,34 +15,67 @@ func main() {
 		Permissions: []*oneroll.Permission{oneroll.Permissions["Super"]},
 	}
 
-	f := new(oneroll.Power)
-	f.NewPower("Flight")
+	f := oneroll.NewPower("Telekinisis")
 
 	f.Dice = &oneroll.DiePool{
 		Normal: 4,
 		Hard:   2,
 	}
 
-	e := oneroll.Extras["Area"]
-	e2 := oneroll.Extras["Go First"]
+	e := oneroll.Modifiers["Area"]
+
+	e.Level = 3
+	e.Cost = e.ModifierCost()
+
+	e2 := oneroll.Modifiers["Go First"]
+	e2.Cost = e2.ModifierCost()
+
+	a := oneroll.Quality{
+		Type:        "Attack",
+		Description: "TK Blast",
+		Level:       3,
+		CostPerDie:  2,
+		Modifiers:   []*oneroll.Modifier{e},
+	}
+
+	rng := oneroll.Capacity{
+		Type:  "Range",
+		Value: "500m",
+	}
+
+	speed := oneroll.Capacity{
+		Type:  "Speed",
+		Value: "250kph",
+	}
 
 	u := oneroll.Quality{
 		Type:        "Useful",
 		Description: "Fly",
 		Level:       1,
 		CostPerDie:  2,
-		Extras:      []*oneroll.Extra{e, e2},
+		Modifiers:   []*oneroll.Modifier{e2},
 	}
 
+	u.Capacities = []*oneroll.Capacity{
+		&speed,
+	}
+
+	a.Capacities = []*oneroll.Capacity{
+		&rng,
+	}
+
+	a.CostPerDie = a.QualityCost()
 	u.CostPerDie = u.QualityCost()
 
-	f.Qualities = []*oneroll.Quality{&u}
+	f.Qualities = []*oneroll.Quality{&a, &u}
 
 	f.Cost = f.PowerCost()
 
+	f.Effect = "Fly and throw TK blasts."
+
 	c.Powers = map[string]*oneroll.Power{
-		"Flight": f}
+		"Telekinisis": f}
 
 	fmt.Println(c)
-	fmt.Println(c.Powers["Flight"])
+	fmt.Println(c.Powers["Telekinisis"])
 }
