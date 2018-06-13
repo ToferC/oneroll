148c08312ad123cc755802bb7dc1a0a72f40f027
toferc <cgeist7@gmail.com>
2018-06-13 08:27:49 -0400

    pre-interface commit

---
+++ tests/testinterface.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+  "fmt"
+  "github.com/toferc/oneroll"
+)
+
+type formatter interface {
+  dieFormat() string
+}
+
+type St struct {
+  Dice *oneroll.DiePool
+}
+
+func (s *St) dieFormat() string {
+
+actions := 1
+  normal := s.Dice.Normal
+	hard := s.Dice.Hard
+	wiggle := s.Dice.Wiggle
+	goFirst := s.Dice.GoFirst
+	spray := s.Dice.Spray
+
+	text := fmt.Sprintf("%dac+%dd+%dhd+%dwd+%dgf+%dsp",
+		actions,
+		normal,
+		hard,
+		wiggle,
+		goFirst,
+		spray)
+
+	return text
+}
+
+type Po struct {
+  Dice *oneroll.DiePool
+}
+
+func (p *Po) dieFormat() string {
+
+actions := 1
+  normal := p.Dice.Normal
+	hard := p.Dice.Hard
+	wiggle := p.Dice.Wiggle
+	goFirst := p.Dice.GoFirst
+	spray := p.Dice.Spray
+
+	text := fmt.Sprintf("%dac+%dd+%dhd+%dwd+%dgf+%dsp",
+		actions,
+		normal,
+		hard,
+		wiggle,
+		goFirst,
+		spray)
+
+	return text
+}
+
+func printFormat(f formatter) string {
+  return f.dieFormat()
+}
+
+func main() {
+  p := Po{Dice: &oneroll.DiePool{
+    Normal: 4,
+    Hard: 2,
+    },
+  }
+  fmt.Println(printFormat(&p))
+
+  s := St{Dice: &oneroll.DiePool{
+      Normal: 2,
+      Hard: 4,
+      },
+    }
+  fmt.Println(printFormat(&s))
+}
